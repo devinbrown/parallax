1391571cce810701bc16af43333478d3b46078e6
Devin Brown <devin.g.brown@gmail.com>
2013-11-02 11:15:56 -0600

    hey, look over there!

--- stage/js/parallax.js
+++ stage/js/parallax.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 
   updateCurtainHeight();
 
+
+
+
   $(window).resize(function() {
     updateCurtainHeight();
   });
@@ -10,3 +13,7 @@ $(document).ready(function() {
     $('#curtain').height($(window).height())
   }
 });
+
+window.onload = function() {
+  $('#curtain #banner').delay(500).fadeIn(1200);
+}
