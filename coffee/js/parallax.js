e6ecae0074d79d4b9d35eaae685710026a769d87
Devin Brown <devin.g.brown@gmail.com>
2013-11-01 20:24:08 -0600

    FIX

---
+++ coffee/js/parallax.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(window).bind('scroll',function(e){
+    parallaxScroll();
+  });
+});
+
+function parallaxScroll(){
+  var bgScrollRate = 0.25;
+  var scrolled = $(window).scrollTop();
+  $('#bg-1').css('top',(0-(scrolled*bgScrollRate))+'px');
+  $('#bg-2').css('top',(0-(scrolled*bgScrollRate))+'px');
+  $('#bg-3').css('top',(0-(scrolled*bgScrollRate))+'px');
+}
