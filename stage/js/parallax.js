7b7824344498863588c40e3f612aa8924555c9d8
Devin Brown <devin.g.brown@gmail.com>
2013-11-02 09:34:18 -0600

    perfect...

--- stage/js/parallax.js
+++ stage/js/parallax.js
@@ -6,18 +6,7 @@ $(document).ready(function() {
     updateCurtainHeight();
   });
 
-  $(window).scroll(function() {
-    scaleImage();
-  });
-
   function updateCurtainHeight() {
     $('#curtain').height($(window).height())
-  }
-
-  function scaleImage() {
-    var scrolled     = $(window).scrollTop();
-    var bgScrollRate = 0.001;
-
-    $('#map-translate').css('transform', 'scale(' + scrolled*bgScrollRate + ',' + scrolled*bgScrollRate + ')');
   }
 });
