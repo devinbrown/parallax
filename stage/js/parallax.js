260d6bf0076bbaa9365823e35aeeba7b0da403eb
Devin Brown <devin.g.brown@gmail.com>
2013-11-06 22:02:20 -0700

    Faster zoom.

--- stage/js/parallax.js
+++ stage/js/parallax.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
   }
 
   function zoomImage(scrolled, target) {
-    var zoomRate = (scrolled / 4000) + 1;
+    var zoomRate = (scrolled / 3500) + 1;
     if (zoomRate > 1) {
       $(target).css('transform', 'scale(' + zoomRate + ',' + zoomRate + ')');
       $(target).css('-ms-transform', 'scale(' + zoomRate + ',' + zoomRate + ')');
